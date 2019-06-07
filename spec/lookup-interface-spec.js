a47b001dd598a5a7d269d53305f2316e08c2bf17
Marc Davies <marcdaviesriot@gmail.com>
2019-06-07 09:38:07 -0700

    add boilerplate content to lookup-interface-spec.js

---
+++ spec/lookup-interface-spec.js
@@ -0,0 +1,10 @@
+import { DoctorLookup } from './../src/lookup-service.js';
+
+describe('DoctorLookup', function() {
+
+    it('should test something', function ()
+    {
+        //test goes here
+    });
+
+});
