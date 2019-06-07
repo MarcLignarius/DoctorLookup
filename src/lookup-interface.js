a258b5a213fad44a3b789dccd1ebaf1b5511ef93
Marc Davies <marcdaviesriot@gmail.com>
2019-06-07 16:07:40 -0700

    update Request URL

--- src/lookup-interface.js
+++ src/lookup-interface.js
@@ -16,16 +16,16 @@ $(document).ready(function() {
     let promise = doctorLookup.findDoctor(name, symptom);
     promise.then(function(response) {
       let body = JSON.parse(response);
-      if (body.length === 0)
+      if (body.data.length === 0)
       {
-        $('#showDoctors').text(`Your search did not return any results. Please try another search.`)
+        $('#showDoctors').text(`Your search did not return any results. Please try another search.`);
       } else {
-        body.forEach(function(doctor) {
-          $('#showDoctors').append(`<strong>${doctor.name}</strong><br> ${doctor.street}<br>${doctor.city}, ${doctor.state}, ${doctor.postal_code}<br> ${doctor.phone}<br> <a href='${doctor.website_url}'>${doctor.website_url}</a><hr>`)
+        body.data.forEach(function(doctor) {
+          $('#showDoctors').append(`<strong>${doctor.name}</strong><br> ${doctor.street}<br>${doctor.city}, ${doctor.state}, ${doctor.postal_code}<br> ${doctor.phone}<br> <a href='${doctor.website_url}'>${doctor.website_url}</a><hr>`);
         }, function(error) {
           $('#showError').text(`There was an error processing your request: ${error.message}`);
-        })
+        });
       }
-    })
-  })
+    });
+  });
 });
--- src/lookup-service.js
+++ src/lookup-service.js
@@ -1,9 +1,11 @@
+`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&user_location=45.523064%2C%20-122.676483&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
 export class DoctorLookup
 {
   findDoctor(name, symptom) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&user_location=45.523064%2C%20-122.676483&sort=full-name-asc&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
