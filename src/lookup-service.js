e657a771649364da36668501cd71f37b2402e47a
Marc Davies <marcdaviesriot@gmail.com>
2019-06-07 11:13:11 -0700

    add code for findDoctor function

--- src/lookup-service.js
+++ src/lookup-service.js
@@ -1,9 +1,18 @@
 export class DoctorLookup
 {
-
-  someFunction()
-  {
-    // function code here
+  findDoctor(name, healthIssue) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = //TBD;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
-
 }
