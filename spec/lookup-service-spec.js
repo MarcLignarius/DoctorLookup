40c9cda740814ab56d5b90c1513e664a65323e57
Marc Davies <marcdaviesriot@gmail.com>
2019-06-07 09:30:41 -0700

    initial commit of spec/ directory, replace template content, add test file for lookup-interface

---
+++ spec/lookup-service-spec.js
@@ -0,0 +1,10 @@
+import { DoctorLookup } from './../src/doctor-lookup.js';
+
+describe('DoctorLookup', function() {
+
+    it('should test something', function ()
+    {
+        //test goes here
+    });
+
+});
